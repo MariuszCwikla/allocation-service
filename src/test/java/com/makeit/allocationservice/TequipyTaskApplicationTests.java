7ff0971e7d6741b13d28a5ae8d040df0d0011bc7
mariusz <mariuszcwikla@gmail.com>
2026-04-15 14:00:53 +0200

    init project

---
+++ src/test/java/com/makeit/allocationservice/TequipyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeit.allocationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TequipyTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
